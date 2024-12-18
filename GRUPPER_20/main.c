08d1afd2cdc05aab2e14b66903763d4adea3446c
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-18 21:36:11 +0100

    Update main.c

--- GRUPPER_20/main.c
+++ GRUPPER_20/main.c
@@ -1,8 +1,21 @@
-#include "lib/DVA_TEST.h"
+#include "DVA_TEST.h"
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
+#include <stdlib.h>
+
+// Funktion för att hantera avbrottssignaler
+void cleanup(int signum) {
+    printf("Avslutar programmet...\n");
+    close_gpio_chip();
+    exit(0);
+}
 
 int main(int argc, char *argv[]) {
+    // Hantera signaler för korrekt stängning
+    signal(SIGINT, cleanup);
+    signal(SIGTERM, cleanup);
+
     int flag = 0;
     if (argc > 1 && strcmp(argv[1], "verbose") == 0) {
         flag = 1;
@@ -18,6 +31,9 @@ int main(int argc, char *argv[]) {
         printf("Multithread-programmet misslyckades.\n");
         return 1;
     }
+
+    // Stäng GPIO-chip vid avslut
+    close_gpio_chip();
 
     return 0;
 }
