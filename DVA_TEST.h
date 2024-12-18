fce42d21a3a85be9c6137e5d435b9ad216c89fcf
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-18 18:18:29 +0100

    Update DVA_TEST.h

--- DVA_TEST.h
+++ DVA_TEST.h
@@ -5,8 +5,8 @@
 int init(int flag);         // Initialize necessary components with optional verbose output
 int test_leds();           // Test program that blinks LEDs for 10 seconds
 int test_hc595(int fake_temp); // Test program that sets the HC595 to indicate fake temperature
-int test_write();          // Test program that writes a predetermined value to EEPROM
-int test_read(); 
+void test_write();          // Test program that writes a predetermined value to EEPROM
+void test_read(); 
 
 
 #endif // DVA_TEST_H
