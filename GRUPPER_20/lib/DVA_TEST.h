cd3812ea54552226e539492f435da2e4d493261f
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-19 20:50:42 +0100

    Update DVA_TEST.h

--- GRUPPER_20/lib/DVA_TEST.h
+++ GRUPPER_20/lib/DVA_TEST.h
@@ -3,11 +3,7 @@
 #ifndef DVA_TEST_H
 #define DVA_TEST_H
 
-int init(int flag);
 int test_leds();
-int test_hc595(int fake_temp);
-int test_write();
-int test_read();
 int multithread_fun();
 
 #endif // DVA_TEST_H
