b6097df51f4cad57a617f309e1500c64158748aa
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-19 21:04:30 +0100

    Update DVA271_GPIO.h

--- GRUPPER_20/lib/DVA271_GPIO.h
+++ GRUPPER_20/lib/DVA271_GPIO.h
@@ -4,10 +4,7 @@
 #include <stdbool.h>
 
 int hc595_init();
-int temp_indicate(float temperature);
-int gpio_init(int pin, bool output);
 int flip_pin(int pin);
-float get_cpu_temperature();
 int set_led_state(int led1, int led2);
 
 // New functions for Write Protect (WP) handling
