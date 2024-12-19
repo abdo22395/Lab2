6091d653a0a61f6cc19b4b40a9f458dcae1b49cc
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-19 21:04:41 +0100

    Update DVA271_GPIO.h

--- GRUPPER_20/lib/DVA271_GPIO.h
+++ GRUPPER_20/lib/DVA271_GPIO.h
@@ -7,8 +7,4 @@ int hc595_init();
 int flip_pin(int pin);
 int set_led_state(int led1, int led2);
 
-// New functions for Write Protect (WP) handling
-int wp_init();       // Initialize the WP pin
-int set_wp(bool enable); // Enable or disable WP (true = enable, false = disable)
-
 #endif // DVA271_GPIO_H
