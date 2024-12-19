a282bd6b59bd37e845c9008f50f4bad9b2c0a24e
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-19 21:37:06 +0100

    Update DVA_TEST.c

--- GRUPPER_20/lib/DVA_TEST.c
+++ GRUPPER_20/lib/DVA_TEST.c
@@ -1,5 +1,3 @@
-// lib/DVA_TEST.c
-
 #include "DVA_TEST.h"
 #include "DVA271_EEPROM.h"
 #include "DVA271_GPIO.h"
@@ -7,12 +5,12 @@
 #include <unistd.h>
 
 int test_leds() {
-    // Blinkar LEDs under 10 sekunder
+    // Blink LEDs for 10 seconds (20 cycles of 250ms on/off)
     for (int i = 0; i < 20; i++) {
         set_led_state(1, 1);
-        usleep(250000); // 250 ms
+        usleep(250000); // 250 ms on
         set_led_state(0, 0);
-        usleep(250000); // 250 ms
+        usleep(250000); // 250 ms off
     }
     return 0;
 }
