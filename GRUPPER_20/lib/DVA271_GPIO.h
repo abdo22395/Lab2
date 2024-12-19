559646facb920c411a5a930e46b0de45cdc75132
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-19 16:56:59 +0100

    Update DVA271_GPIO.h

--- GRUPPER_20/lib/DVA271_GPIO.h
+++ GRUPPER_20/lib/DVA271_GPIO.h
@@ -1,5 +1,3 @@
-// lib/DVA271_GPIO.h
-
 #ifndef DVA271_GPIO_H
 #define DVA271_GPIO_H
 
@@ -11,5 +9,9 @@ int gpio_init(int pin, bool output);
 int flip_pin(int pin);
 float get_cpu_temperature();
 int set_led_state(int led1, int led2);
+
+// New functions for Write Protect (WP) handling
+int wp_init();       // Initialize the WP pin
+int set_wp(bool enable); // Enable or disable WP (true = enable, false = disable)
 
 #endif // DVA271_GPIO_H
