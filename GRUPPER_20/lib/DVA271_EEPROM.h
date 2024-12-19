9ae59e607c2eb939bc937c75e91fa70629346d88
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-19 16:57:09 +0100

    Update DVA271_EEPROM.h

--- GRUPPER_20/lib/DVA271_EEPROM.h
+++ GRUPPER_20/lib/DVA271_EEPROM.h
@@ -1,5 +1,3 @@
-// lib/DVA271_EEPROM.h
-
 #ifndef DVA271_EEPROM_H
 #define DVA271_EEPROM_H
 
@@ -9,5 +7,10 @@ int write_joke(char arr[255], int joke_length);
 int write_joke_pos(char arr[255], int joke_length, int pos);
 int clear_eeprom(int ki_length);
 int fill_eeprom(int ki_length);
+
+// Optional: Add WP control functions here for direct EEPROM-level abstraction
+// These would call the GPIO functions internally if needed
+// int enable_eeprom_wp();
+// int disable_eeprom_wp();
 
 #endif // DVA271_EEPROM_H
