ea6c76d16dfb38b3b2680d92bd8ae0a8b6e82196
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-19 20:51:05 +0100

    Update DVA_TEST.c

--- GRUPPER_20/lib/DVA_TEST.c
+++ GRUPPER_20/lib/DVA_TEST.c
@@ -6,29 +6,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// Variabel för verbose-läge
-static int verbose_mode = 0;
-
-int init(int flag) {
-    verbose_mode = flag;
-
-    // Initialisera EEPROM
-    if (eeprom_setup() != 0) {
-        if (verbose_mode) printf("eeprom_setup returned FAILURE\n");
-        return 1;
-    }
-    if (verbose_mode) printf("eeprom_setup returned SUCCESS\n");
-
-    // Initialisera HC595
-    if (hc595_init() != 0) {
-        if (verbose_mode) printf("hc595_init returned FAILURE\n");
-        return 2;
-    }
-    if (verbose_mode) printf("hc595_init returned SUCCESS\n");
-
-    return 0;
-}
-
 int test_leds() {
     // Blinkar LEDs under 10 sekunder
     for (int i = 0; i < 20; i++) {
@@ -37,39 +14,5 @@ int test_leds() {
         set_led_state(0, 0);
         usleep(250000); // 250 ms
     }
-    return 0;
-}
-
-int test_hc595(int fake_temp) {
-    // Sätter registret med fake_temp
-    if (temp_indicate((float)fake_temp) != 0) {
-        return 1;
-    }
-    return 0;
-}
-
-int test_write() {
-    // Skriver ett förbestämt skämt till EEPROM
-    char joke[255] = "Detta är ett testskämt.";
-    if (write_joke(joke, strlen(joke)) != 0) {
-        return 1;
-    }
-    return 0;
-}
-
-int test_read() {
-    // Läser det första skämtet från EEPROM
-    char* joke;
-    if (get_joke(0, &joke) != 0) {
-        return 1;
-    }
-    printf("Testläsning av skämt: %s\n", joke);
-    free(joke);
-    return 0;
-}
-
-int multithread_fun() {
-    // Denna funktion är implementerad i main.c
-    // Här kan du kalla andra funktioner för testning om det behövs
     return 0;
 }
